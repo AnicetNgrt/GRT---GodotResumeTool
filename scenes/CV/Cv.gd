0c65209709192c3675c5dad49b74b57fe37f0eda
Anicet Nougaret <anicetnougaret@gmail.com>
2020-09-28 20:05:55 +0200

    improved CV

--- scenes/CV/Cv.gd
+++ scenes/CV/Cv.gd
@@ -1,12 +1,13 @@
 extends Control
 
-
 func _input(event):
 	if Input.is_action_just_pressed("screenshot"):
 		var img = get_viewport().get_texture().get_data()
+		img.flip_y()
 		yield(get_tree(), "idle_frame")
 		yield(get_tree(), "idle_frame")
 		img.save_png("user://CV"+str(randi()%50)+".png")
+		print(OS.get_user_data_dir())
 	if Input.is_action_just_pressed("switch"):
 		$GDEVversion.visible = !$GDEVversion.visible
 		$PROGversion.visible = !$PROGversion.visible
